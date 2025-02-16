bb6196d35984c2ea1806ba47becb81b1d7d53427
Artem Hloba <artemgloba2@gmail.com>
2025-02-16 09:24:04 +0000

    Add some props and typing

---
+++ src/app/components/core/moc_data/tasks.ts
@@ -0,0 +1,15 @@
+import { Task } from '../models/task.model'
+
+export const tasks: Task[] = [
+  {
+    title: 'Download Angular',
+    assignee: 'Artem',
+    dueDate: '16.02.2025'
+  },
+  {
+    title: 'Download Angular',
+    description: 'Some text',
+    assignee: 'Artem',
+    dueDate: '16.02.2025'
+  }
+]
